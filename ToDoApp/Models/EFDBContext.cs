64761659d5a5f9a2b6f61ddb57e4c2e3851d9633
Karchage <glebkul2010@yandex.ru>
2020-09-18 23:15:12 +0300

    change

--- ToDoApp/Models/EFDBContext.cs
+++ ToDoApp/Models/EFDBContext.cs
@@ -13,7 +13,6 @@ namespace ToDoApp.Models
         public DbSet<ToDo> ToDo { get; set; }
         public EFDBContext(DbContextOptions<EFDBContext> options) :base(options)
         {
-            Database.EnsureCreated();
         }
     }
 }
